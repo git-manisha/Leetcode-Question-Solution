e3747e44fa09a92b662bd249febfcf515ae2ff2a
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2024-11-02 09:24:09 +0530

    Time: 0 ms (100%), Space: 14.3 MB (36.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int remain = target-nums[i];
+            if(mp.find(remain) != mp.end()){
+                ans.push_back(mp[remain]);
+                ans.push_back(i);
+                break;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
