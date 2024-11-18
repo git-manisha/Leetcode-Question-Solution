e10b29439c670d7604c7212361ced4b44c740e2e
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2024-11-18 12:39:41 +0530

    Time: 18 ms (5%), Space: 19 MB (11.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    typedef pair<int,int> p;
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
-        for(auto it:nums){
-            mp[it]++;
+        for(int i:nums){
+            mp[i]++;
         }
+        vector<vector<int>> v(nums.size()+1);
+        for(auto &it:mp){
+            int value = it.first;
+            int freq = it.second;
 
-        vector<vector<int>> temp(nums.size()+1);
-        for(auto it:mp){
-            temp[it.second].push_back(it.first);
+            v[freq].push_back(value);
         }
-        int n = nums.size();
+        int n = v.size()-1;
         vector<int> result;
         for(int i=n;i>=0;i--){
-            if(temp[i].size()==0){
+            if(v[i].size() == 0){
                 continue;
             }
-            while(temp[i].size()>0 && k>0){
-                result.push_back(temp[i].back());
-                temp[i].pop_back();
+            while(v[i].size()>0 && k>0){
+                result.push_back(v[i].back());
+                v[i].pop_back();
                 k--;
             }
         }
