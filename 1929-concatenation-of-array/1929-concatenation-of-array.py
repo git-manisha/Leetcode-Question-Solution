d891275e79cf0b4b4ed5d0639c6f6abaf47e4b4b
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2024-12-13 13:59:17 +0530

    Time: 0 ms (100%), Space: 12.4 MB (14.57%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -4,11 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        size = len(nums)
-        i =0;
-        while(i<size):
-            nums.append(nums[i])
-            i = i+1
-        
+        nums = nums+nums
         return nums
-        
