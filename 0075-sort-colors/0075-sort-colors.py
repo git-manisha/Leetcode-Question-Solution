482498e23ade41d069349ef14ed8968822bdbf65
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2025-01-01 21:39:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,8 +1,24 @@
+def swap(nums,j,k):
+    temp = nums[j]
+    nums[j] = nums[k]
+    nums[k] = temp
 class Solution(object):
     def sortColors(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-        
+        i=0
+        j=0
+        k = len(nums)-1
+
+        while(i<k and j<=k and i<=j):
+            if(nums[j] == 2):
+                swap(nums,j,k)
+                k -=1
+            elif(nums[j]==0):
+                swap(nums,j,i)
+                j +=1
+                i +=1
+            else:
+                j +=1
