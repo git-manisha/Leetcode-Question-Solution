0de926b1754d730d53ad109d58d0c742f153b65e
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2025-01-17 20:21:54 +0530

    Time: 682 ms (67.65%), Space: 25 MB (100%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -5,20 +5,17 @@ class Solution(object):
         :rtype: List[int]
         """
         presum = 0
-        l1 = []
+        total = 0
         for itr in nums:
-            presum +=itr
-            l1.append(presum)
+            total +=itr
 
         i=0
         sum1 =0
         s = len(nums)-1
         while(i<len(nums)):
-            nums[i] = ((nums[i]*i)-(l1[i]-nums[i]))+((l1[s]-l1[i])-(nums[i]*(len(nums)-i-1)))
+            temp = ((nums[i]*i)-presum)+((total-presum-nums[i])-(nums[i]*(len(nums)-i-1)))
+            presum +=nums[i]
+            nums[i] = temp
             i +=1
         
         return nums
-
-
-            
-
