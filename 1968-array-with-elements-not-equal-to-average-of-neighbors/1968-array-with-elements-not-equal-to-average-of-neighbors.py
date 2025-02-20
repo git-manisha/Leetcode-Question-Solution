4caacfa0586405d09bb23211f3db2cfb4fb47e02
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2025-02-20 12:08:58 +0530

    Time: 159 ms (88.46%), Space: 28.9 MB (98.08%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums.sort()
-        l1 = [0]*len(nums)
-        i =0
-        j=0
-        while(i<len(nums)):
-            l1[i] = nums[j]
-            i +=2
-            j +=1
-        i =1
-        while(i<len(nums)):
-            l1[i] = nums[j]
-            i +=2
-            j +=1
+        i=1
+        while(i<len(nums)-1):
+            if((nums[i-1]+nums[i+1]) == nums[i]*2):
+                nums[i],nums[i+1] = nums[i+1],nums[i]
+            i +=1
         
-        return l1
+        j = len(nums)-2
+        while(j>0):
+            if((nums[j+1]+nums[j-1]) == nums[j]*2):
+                nums[j],nums[j+1] = nums[j+1],nums[j]
+            j -=1
         
-        
-
+        return nums
         
