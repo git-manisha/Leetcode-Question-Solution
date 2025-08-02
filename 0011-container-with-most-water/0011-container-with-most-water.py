a1a23f1799e58049a4c12e2450ab355860c9d2c2
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2025-08-02 13:23:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,19 +4,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        i=0
-        j=len(height)-1
-        max1 = (j-i)*min(height[i],height[j])
-
-        while(i<j):
-            if(height[i]<height[j]):
-                i +=1
+        first = 0
+        last = len(height)-1
+        while(first<last):
+            if(height[first]<height[last]):
+                height[first] = (last-first)*height[first]
+                first +=1
             else:
-                j -=1
-            max1 = max(((j-i)*min(height[i],height[j])),max1)
-        
-
-        return max1
-           
-          
+                height[last] = (last-first)*height[last]
+                last -=1
+            
+        return max(height)
+                
         
