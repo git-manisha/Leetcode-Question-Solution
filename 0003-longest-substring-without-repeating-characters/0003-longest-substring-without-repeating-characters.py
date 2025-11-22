eacd41ed988d921d442c1db213dba4c87118aa81
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2025-11-22 16:05:33 +0530

    Time: 27 ms (42.64%), Space: 12.6 MB (69.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,25 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        substring = ""
+        right = 0
+        left = 0
         hashmap = {}
         max_length = 0
-        first = 0
-        last = 0
-        while(last < len(s)):
-            if(s[last] in hashmap):
-                max_length = max(max_length,len(substring))
-                while(s[first] != s[last]):
-                    del hashmap[s[first]]
-                    substring = substring[1:]
-                    first +=1
-                substring = substring[1:]
-                first +=1
-                substring +=s[last]
+
+        while(left<len(s)):
+            if(s[left] in hashmap):
+                while(s[right] != s[left]):
+                    del hashmap[s[right]]
+                    right +=1
+                right +=1
+                max_length = max(max_length,(left-right+1))
             else:
-                substring +=s[last]
-                hashmap[s[last]] = 1
-            last +=1
-        max_length = max(max_length,len(substring))
+                hashmap[s[left]] = 1
+                max_length = max(max_length,(left-right+1))
+            left +=1
+        
+        #max_length = max(max_length,(left-right+1))
+
         return max_length
-            
