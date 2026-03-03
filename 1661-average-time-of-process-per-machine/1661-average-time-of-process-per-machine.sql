35c4e7a2294be8487f75b0eb8c811051da936111
Manisha Soni <132186518+git-manisha@users.noreply.github.com>
2026-03-03 23:09:21 +0530

    Time: 281 ms (14.28%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select machine_id,round((sum(timep)/count(process_id)),3) as processing_time
 from (
-select a1.machine_id,a1.process_id,(a1.timestamp - a2.timestamp) as timep
+select a1.machine_id,a1.process_id,abs(a1.timestamp - a2.timestamp) as timep
 from Activity as a1
 inner join Activity as a2
 where a1.process_id = a2.process_id
